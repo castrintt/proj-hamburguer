63f912f9351a73fc93021d13129cf95938be2741
castrintt <igordc38@gmail.com>
2022-04-12 23:22:08 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <Navbar :logo="logo_source" :alt="app_name"/>
+    <router-view></router-view>
+    <Footer/>
+  </div>
 </template>
 
+
+<script>
+import Navbar from "./components/Navbar.vue"
+import Footer from './components/Footer.vue'
+export default {
+  name:'App',
+  data(){
+    return{
+      logo_source: '/img/logo.png',
+      app_name:'Make your burguer'
+      
+    }
+  },
+  components:{
+    Navbar,
+    Footer
+  }
+}
+</script>
+
+ 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
